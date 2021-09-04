9dc94a894e795e2dbbb8b45af60a0adb07e803a1
gnragazzi <gnragazzi@gmail.com>
2021-09-04 14:07:58 -0300

    Complete challenge tested with client React

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,7 @@
 
 'use strict';
 
-const { addNewBook, getAllBooks } = require("../lib/db");
+const { addNewBook, getAllBooks, getBookById, commentBook, deleteBookById, deleteAllBooks } = require("../lib/db");
 
 module.exports = function (app) {
 
@@ -29,7 +29,10 @@ module.exports = function (app) {
     })
     
     .delete(function(req, res){
-      //if successful response will be 'complete delete successful'
+      deleteAllBooks().then(books=>{
+        if(!books) return res.send('no book exists')
+        return res.send('complete delete successful')
+      }).catch(err=>res.send('no book exists'))
     });
 
 
@@ -37,18 +40,35 @@ module.exports = function (app) {
   app.route('/api/books/:id')
     .get(function (req, res){
       let bookid = req.params.id;
-      //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+      getBookById(bookid).then(book=>{
+        if(!book) return res.send('no book exists')
+        return res.json(book)
+      }).catch(err=>res.send('no book exists'))
     })
     
     .post(function(req, res){
       let bookid = req.params.id;
       let comment = req.body.comment;
-      //json res format same as .get
+      if(!comment) return res.send('missing required field comment')
+      getBookById(bookid).then(book=>{
+        if(!book) return res.send('no book exists')
+        const {comments} = book
+        comments.push(comment)
+        const commentcount = comments.length
+        commentBook(bookid,comments, commentcount)
+          .then(book=>{
+            return res.json(book)
+          })
+          .catch(err=>res.send(`no book exists`))
+      }).catch(err=>res.send(`no book exists`))
     })
     
     .delete(function(req, res){
       let bookid = req.params.id;
-      //if successful response will be 'delete successful'
+      deleteBookById(bookid).then(book=>{
+        if(!book) return res.send('no book exists')
+        return res.send('delete successful')
+      }).catch(err=>res.send('no book exists'))
     });
   
 };
